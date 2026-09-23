e338fa3832f1dc72ccec4c178acfd0862b956b11
Slightly-Deranged-Lunatic <110319723+Slightly-Deranged-Lunatic@users.noreply.github.com>
2026-09-23 13:12:11 -0500

    Formatting changes and stuff

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,6 +1,6 @@
 use std::process::Command;
-use std::time;
 use std::thread;
+use std::time;
 
 fn main() {
     let device = "Keychron  Keychron Link "; // YES THE SPACE AT THE END MATTERS I DONT KNOW WHY ITS DUMB
@@ -19,7 +19,7 @@ fn main() {
             thread::sleep(sleep_time);
         }
 
-        if ! sober_open(&window_to_search_for) {
+        if !sober_open(&window_to_search_for) {
             disable_preset(device);
         }
     }
@@ -27,33 +27,29 @@ fn main() {
 
 // Gets the window ID's of all currently opened windows
 fn get_window_ids() -> Vec<String> {
-    let command_output = Command::new("kdotool")
-        .arg("search")
-        .output()
-        .unwrap();
+    let command_output = Command::new("kdotool").arg("search").output().unwrap();
 
     let mut output = String::new();
 
     if command_output.status.success() {
-        output = String::from_utf8(command_output.stdout)
-            .expect("String was not a valid UTF 8");
+        output = String::from_utf8(command_output.stdout).expect("String was not a valid UTF 8");
     } else {
         panic!("kdotool was not a success, output: {:?}", command_output);
     }
-    let window_ids: Vec<String> = output.lines().map(String::from).collect();
-    return window_ids
+
+    output.lines().map(String::from).collect()
 }
 
 fn enable_preset(device: &str) {
     let preset = "DVN";
 
-    let output = Command::new("input-remapper-control")
+    Command::new("input-remapper-control")
         .arg("--command")
         .arg("start")
         .arg("--device")
-        .arg(format!("{}", device))
+        .arg(device)
         .arg("--preset")
-        .arg(format!("{}", preset))
+        .arg(preset)
         .output()
         .unwrap();
 }
@@ -63,23 +59,23 @@ fn disable_preset(device: &str) {
         .arg("--command")
         .arg("stop")
         .arg("--device")
-        .arg(format!("{}", device))
+        .arg(device)
         .output();
 }
 
 fn sober_open(window_to_search_for: &String) -> bool {
     let window_ids = get_window_ids();
 
-
     for id in window_ids {
         let window_name = String::from_utf8(
             Command::new("kdotool")
-            .arg("getwindowname")
-            .arg(format!("{}", id))
-            .output()
-            .unwrap()
-            .stdout
-        ).unwrap();
+                .arg("getwindowname")
+                .arg(id)
+                .output()
+                .unwrap()
+                .stdout,
+        )
+        .unwrap();
         if window_name.trim() == window_to_search_for {
             return true;
         }
